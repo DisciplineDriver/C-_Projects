9d5144a358087394d128f68401692a2ed22f313b
DisciplineDriver <chrisalexr05@icloud.com>
2024-12-12 12:02:47 -0600

    Create mathlibs.cpp

---
+++ mathlibs.cpp
@@ -0,0 +1,31 @@
+/*#include<iostream>
+
+#include<cmath>  // for functions exp,sqrt
+#include<iomanip> // setprecision
+using namespace std;
+
+#define PI 3.14159
+
+int main() {
+
+	// round e^x to the fifth decimal
+	cout << setprecision(8);
+	cout << "e ^ x yields " << exp(PI) << endl;
+
+	// calc any perfect squares between 1 through 100
+
+	for (int i = 1; i <= 100; i++) {
+
+		cout << "Value of " << i << " squared is  " << pow(i, 2)
+			<< " and the sqrt of " << i << " is " << sqrt(i);
+		if(sqrt(i) == int(sqrt(i)))
+		cout << " : Is a Perf square! " << endl;
+		cout << endl;
+
+
+
+	}
+
+	cout << endl;
+
+}*/
